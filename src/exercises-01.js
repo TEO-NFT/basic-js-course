9531083f269b21f9441126787d85a7b81686f7c7
Mateo <mateo.orolot@gmail.com>
2023-01-22 19:16:00 -0500

    Add exercise 01

---
+++ src/exercises-01.js
@@ -0,0 +1,25 @@
+// Declaring variables 
+
+let nickName;
+let lastName;
+
+
+// Initializing variables 
+
+nickName = "TEO";
+lastName = "NFT";
+
+// Declaring and initializing variables
+
+let isActive = true;
+let moneySaved = 7500;
+
+// Re initializing variables
+
+isActive = false;
+moneySaved = 7000;
+
+// Declare variables of type const for values that will not change 
+
+const price = 57000;
+const games = 27;
